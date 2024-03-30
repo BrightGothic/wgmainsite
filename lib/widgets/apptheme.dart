c215602ecf9d16777a439491cbe316bbe756712d
BrightGothic <83520055+BrightGothic@users.noreply.github.com>
2024-03-31 00:44:30 +0100

    fix theme

--- lib/widgets/apptheme.dart
+++ lib/widgets/apptheme.dart
@@ -4,7 +4,7 @@ final ThemeData appTheme = ThemeData(
   primarySwatch: Colors.blue,
   scaffoldBackgroundColor: Color(0xFF1E1D1D),
   textTheme: TextTheme(
-    headline6: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-    bodyText2: TextStyle(fontSize: 16.0, color: Colors.white),
+    titleLarge : TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    bodyMedium : TextStyle(fontSize: 16.0, color: Colors.white),
   ),
 );
