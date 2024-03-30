f70c477b6512afad32e1ac55a136b7b1570eef38
BrightGothic <83520055+BrightGothic@users.noreply.github.com>
2024-03-31 00:50:46 +0100

    Improved the home page architecture

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -37,117 +37,115 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       theme: appTheme,
       home: Scaffold(
-        body: Expanded(
-          child: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 100.0),
-                    child: Container(),
-                  ),
-                  Text("Time remaining before explosion : ",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
-                  Flipdownclock(),
-                  Padding(
-                    padding: EdgeInsets.only(top: 100.0),
-                    child: Container(),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        showWidget = !showWidget;
-                      });
-                    },
-                    child: Text('Very secret button, DO NOT CLICK',
-                        style: TextStyle(color: Colors.grey)),
-                    style: raisedButtonStyle2,
-                  ),
-                  if (showWidget)
-                    SizedBox(
-                      height: 400,
-                      child: Center(
-                        child: Column(children: [
-                          Padding(padding: EdgeInsets.only(top: 60.0)),
-                          Text(
-                              "In case of emergency, enter the secret code to stop the bomb"),
-                          Form(
-                            key: _formKey,
-                            child: SizedBox(
-                              width: 400,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  TextFormField(
-                                    controller: secretbombcontroller,
-                                    style: new TextStyle(
-                                        fontSize: 22.0, color: Colors.white),
-                                    // The validator receives the text that the user has entered.
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter some text';
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 16),
-                                    child: Center(
-                                      child: ElevatedButton(
-                                        onPressed: () {
-                                          // Validate returns true if the form is valid, or false otherwise.
-                                          if (_formKey.currentState!
-                                              .validate()) {
-                                            // If the form is valid, display a snackbar. In the real world,
-                                            // you'd often call a server or save the information in a database.
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 100.0),
+                  child: Container(),
+                ),
+                Text("Time remaining before explosion : ",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
+                Flipdownclock(),
+                Padding(
+                  padding: EdgeInsets.only(top: 100.0),
+                  child: Container(),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      showWidget = !showWidget;
+                    });
+                  },
+                  child: Text('Very secret button, DO NOT CLICK',
+                      style: TextStyle(color: Colors.grey)),
+                  style: raisedButtonStyle2,
+                ),
+                if (showWidget)
+                  SizedBox(
+                    height: 400,
+                    child: Center(
+                      child: Column(children: [
+                        Padding(padding: EdgeInsets.only(top: 60.0)),
+                        Text(
+                            "In case of emergency, enter the secret code to stop the bomb"),
+                        Form(
+                          key: _formKey,
+                          child: SizedBox(
+                            width: 400,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                TextFormField(
+                                  controller: secretbombcontroller,
+                                  style: new TextStyle(
+                                      fontSize: 22.0, color: Colors.white),
+                                  // The validator receives the text that the user has entered.
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 16),
+                                  child: Center(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        // Validate returns true if the form is valid, or false otherwise.
+                                        if (_formKey.currentState!
+                                            .validate()) {
+                                          // If the form is valid, display a snackbar. In the real world,
+                                          // you'd often call a server or save the information in a database.
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                                content:
+                                                    Text('Processing Data')),
+                                          );
+                                          //password : ViVeriVeniversumVivusVici
+                                          if (md5.convert(utf8.encode(secretbombcontroller.text)).toString() =="3e926a1c73256bb284001bdab8e5c265") {
+                                            setState(() {
+                                              bombstopped = !bombstopped;
+                                            });
                                             ScaffoldMessenger.of(context)
                                                 .showSnackBar(
                                               const SnackBar(
                                                   content:
-                                                      Text('Processing Data')),
+                                                  Text('Bomb diffused')),
                                             );
-                                            //password : ViVeriVeniversumVivusVici
-                                            if (md5.convert(utf8.encode(secretbombcontroller.text)).toString() =="3e926a1c73256bb284001bdab8e5c265") {
-                                              setState(() {
-                                                bombstopped = !bombstopped;
-                                              });
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(
-                                                const SnackBar(
-                                                    content:
-                                                    Text('Bomb diffused')),
-                                              );
-                                            }else {
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(
-                                                const SnackBar(
-                                                    content:
-                                                    Text('Wrong password')),
-                                              );
-                                            }
+                                          }else {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              const SnackBar(
+                                                  content:
+                                                  Text('Wrong password')),
+                                            );
                                           }
-                                        },
-                                        child: const Text('Submit'),
-                                      ),
+                                        }
+                                      },
+                                      child: const Text('Submit'),
                                     ),
                                   ),
-                                  if (bombstopped) Text("Bomb stopped !") else Text("Bomb working !")
-                                ],
-                              ),
+                                ),
+                                if (bombstopped) Text("Bomb stopped !") else Text("Bomb working !")
+                              ],
                             ),
                           ),
-                        ]),
-                      ),
-                    )
-                  else
-                    Container(),
+                        ),
+                      ]),
+                    ),
+                  )
+                else
+                  Container(),
 
-                ],
-              ),
+              ],
             ),
           ),
         ),
